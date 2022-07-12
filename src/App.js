e8fa39a5a5a83dc981e4da3799aa55ea5b7ed7bf
Tara <tlee379@uwo.ca>
2022-07-12 11:57:19 -0400

    trying to fix navbar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import './App.css';
 import Home from './Components/Home/Home';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Routes, Route } from "react-router-dom";
 import Navbar from './Components/Navbar/Navbar';
 import Contact from './Components/Pages/Contact/Contact';
-import Portfolio from './Components/Portfolio/Portfolio';
+import Portfolio from './Components/Pages/Portfolio/Portfolio';
 
 function App() {
   return (
     <div>
+      
+      <div>
       <Navbar />
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/Contact' element={<Contact/>} />
         <Route path='/Portfolio' element={<Portfolio/>} />
       </Routes>
+      </div>
   </div>
   );
 }
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,28 +1,42 @@
 import React from 'react'
 import './Navbar.css'
+import {Link, useMatch, useResolvedPath } from "react-router-dom"
 
 function Navbar() {
+  
   return (
     <div className='navbar-container'>
         <div className='logo'>
-            <a href='/'>
+            <Link href='/'>
             <img src='images/logo.png'></img>
-            </a>
+            </Link>
         </div>
         <div className="links-container">
          <ul>
          <li>
-            <a href="/Portfolio">portfolio</a>
+            <Link to="/Portfolio">portfolio</Link>
         </li>
          <li>
-            <a href="/About">about us</a>
+            <CustomLink to="/About">about us</CustomLink>
         </li>
          <li>
-            <a href="/Contact">contact us</a>
+            <CustomLink to="/Contact">contact us</CustomLink>
         </li>
         </ul>
         </div>
     </div>
+  )
+}
+function CustomLink({ to, children, ...props }) {
+  const resolvedPath = useResolvedPath(to)
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true })
+
+  return (
+    <li className={isActive ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
   )
 }
 
