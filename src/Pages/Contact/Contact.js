b98d690cc2b2ef389f297eeca077ec1a83adfabd
Henry Chen <hchen869@uwo.ca>
2022-07-12 19:01:04 -0400

    Update Contact.js

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -4,7 +4,17 @@ import './Contact.css';
 function Contact() {
   return (
     <div >
-        Contact Us
+        &lt;Contact&gt;
+        <div className="Contact-List">
+        Have Any Questions or Comments? Reach out to us through:
+          <ul>
+            <li>Linkedin: Western Developer's Society </li>
+            <li>Instagram: @westerndevsociety</li>
+            <li>Facebook: @westerndevsociety</li>
+            <li>Email: asdfghjk@placeholder.com</li>
+          </ul>
+        </div>
+        &lt;/Contact&gt;
     </div>
   );
 }
