68b9825426122342a22cfcc3099e7055a58d763b
Henry Chen <hchen869@uwo.ca>
2022-07-13 19:35:28 -0400

    CSS Styling and Links for Contact Page

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -6,19 +6,36 @@ import { FaFacebookSquare } from 'react-icons/fa';
 import { FaEnvelope} from 'react-icons/fa';
 
 function Contact() {
+
+  document.body.style = 'background: #B89EC5;';
+
   return (
-    <div >
-        &lt;Contact&gt;
-        <div className="Contact-List">
-        Have Any Questions or Comments? Reach out to us through:
-          <ul>
-            <li>Linkedin: Western Developer's Society <FaLinkedin /> </li>
-            <li>Instagram: @westerndevsociety <FaInstagram /> </li>
-            <li>Facebook: @westerndevsociety <FaFacebookSquare /> </li>
-            <li>Email: asdfghjk@placeholder.com <FaEnvelope/> </li>
-          </ul>
+    <div className="Contact-Container">
+      <div className="Contact-1">
+        <div className="Contact-Title">
+          Let's Chat!
         </div>
-        &lt;/Contact&gt;
+
+        <div className="Contact-Description">
+            Want to learn more? <br />
+            Want us to build you 
+            a beautiful website?
+        </div>
+
+      </div>  
+
+      <div className="Contact-2">
+        <div className="Contact-Stater">   
+        <div className="Contact-Email">   <FaEnvelope/> &gt;write us at asdfghjk@placeholder.com <br /></div>
+        <div className="Contact-check"> &gt;check out our socials! </div>
+        </div>
+        
+        <div className="Contact-Socials">
+          <div className="Contact-linkedin">  <a href="https://www.linkedin.com/company/western-dev-society/?originalSubdomain=ca" target="_blank"><FaLinkedin /></a> <br/> Western Developer's Society</div>
+          <div className="Contact-insta">  <a href="https://www.instagram.com/westerndevsociety/" target="_blank"><FaInstagram /></a>  <br/> @westerndevsociety </div>
+          <div className="Contact-facebook">  <a href="https://www.facebook.com/westerndevsociety/" target="_blank"><FaFacebookSquare /></a> <br/> @westerndevsociety </div>
+        </div>
+      </div>
     </div>
   );
 }
