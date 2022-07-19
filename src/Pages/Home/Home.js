c4cb53c2d2ef62dfec9f3d65ef9ed1c1b591e2d8
Patrick Cui <haoqi.cui@gmail.com>
2022-07-20 05:51:31 +0800

    first commit

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -9,7 +9,9 @@ export default function Home() {
       <div className="content">
         <div className="info">
           <div className="title">
-            Western <br /> Developers <br /> Society
+            <span style={{color:'#9D62CC'}}>Western</span> <br /> 
+            <span style={{color:'#60468C'}}>Developers</span> <br /> 
+            <span style={{color:'#303030'}}>Society</span>
           </div>
           <div className="text">
             Building community, one life of code at a time.
@@ -17,8 +19,9 @@ export default function Home() {
         </div>
 
         <div className="pic">
-          <img src='images/tech.png'></img>
+          <img src='images/tech.png' style={{width:706,height:383}}></img>
         </div>
+      
       </div>
     </div>
 
