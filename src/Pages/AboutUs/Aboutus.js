2ac3b26c3fedd0ed721c978987892bce706146b2
Maaz Ali <maaz@Maazs-MacBook-Air.local>
2022-08-15 23:05:29 -0400

    fixed spacing for contact links

--- src/Pages/AboutUs/Aboutus.js
+++ src/Pages/AboutUs/Aboutus.js
@@ -1,37 +1,28 @@
-import React from 'react'
+import React from "react"
 import Membercards from "./Membercards"
-import data from './data'
-import './Aboutus.css'
-import Fade from 'react-reveal/Fade'
+import data from "./data"
+import "./Aboutus.css"
+import Fade from "react-reveal/Fade"
 
-
-export default function Aboutus(){
-  
-  //document.body.style = 'background: #D9DDF5;';
-
-const cards = data.map(item=>{
-  return (<Membercards
-    key={item.id}
-    item={item}
-  />)
-})
+export default function Aboutus() {
+  const cards = data.map((item) => {
+    return <Membercards key={item.id} item={item} />
+  })
 
   return (
     <div className="aboutus">
-      
-      <div className='about-top-container'>
+      <div className="about-top-container">
         <Fade>
           <div className="aboutus-title">About Us</div>
-            <div className="aboutus-description">
-              At Western Developers Society, we want to give students real life development experience. Join us today!
-            </div>
+          <div className="aboutus-description">
+            At Western Developers Society, we want to give students real life
+            development experience. Join us today!
+          </div>
         </Fade>
       </div>
-      <div className='team-container'>
-          <h1 className="team-title" >Meet the Team!</h1>
-        <div className="card-container">
-          {cards}
-        </div>
+      <div className="team-container">
+        <h1 className="team-title">Meet the Team!</h1>
+        <div className="card-container">{cards}</div>
       </div>
     </div>
   )
