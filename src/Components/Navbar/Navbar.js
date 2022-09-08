6f76c5b70b4103747682f4183d54a65dacbb38dd
Maaz Ali <maaz@Maazs-Air.ht.home>
2022-09-07 23:54:09 -0400

    made nav semi-responsive

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,37 +1,38 @@
-import React from 'react'
-import './Navbar.css'
-import {Link} from "react-router-dom"
-import logo from './logo.png'
+import React from "react"
+import "./Navbar.css"
+import { Link } from "react-router-dom"
+import logo from "./logo.png"
 
 function Navbar() {
-
   return (
-    <div className='navbar-container'>
-        <div className='logo'>
-            <Link to='/'>
-            <img src={logo}></img>
-            </Link>
+    <nav className="navbar">
+      <div className="navbar-container">
+        <div className="logo">
+          <Link to="/">
+            <img src={logo} alt="Dev Society Logo"></img>
+          </Link>
         </div>
         <div className="links-container">
-         <ul>
-        <li>
-            <Link to="/Contact">contact us</Link>
-        </li>
-         <li>
-            <Link to="/JoinUs">join us</Link>
-        </li>
-        <li>
-            <Link to="/AboutUs">about us</Link>
-        </li>
-         <li>
-            <Link to="/Events">events</Link>
-        </li>
-        {/**<li>
-            <Link to="/Portfolio">portfolio</Link>
-        </li> */}
-        </ul>
+          <ul>
+            <li>
+              <Link to="/Contact">contact us</Link>
+            </li>
+            <li>
+              <Link to="/JoinUs">join us</Link>
+            </li>
+            <li>
+              <Link to="/AboutUs">about us</Link>
+            </li>
+            <li>
+              <Link to="/Events">events</Link>
+            </li>
+            {/**<li>
+                <Link to="/Portfolio">portfolio</Link>
+            </li> */}
+          </ul>
         </div>
-    </div>
+      </div>
+    </nav>
   )
 }
 
