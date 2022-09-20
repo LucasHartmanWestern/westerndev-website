dac74fcb81a58bd271b08e814409469fd1ab833a
Tara <tlee379@uwo.ca>
2022-09-20 11:44:42 -0400

    adding media spacing for main page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -18,42 +18,21 @@ export default function Home() {
       <Fade>
         <section className="hero">
           <div className="hero-container">
-            <div className="home-info">
               <div className="title">
                 <span className="line1" style={{ color: "#9D62CC" }}>
                   Western
                 </span>
-                <br />
                 <span className="line2" style={{ color: "#60468C" }}>
                   Developers
                 </span>
-                <br />
                 <span className="line3" style={{ color: "#303030" }}>
                   Society
                 </span>
               </div>
-            </div>
             <div className="pic">
                 <img src={tech} alt="Dev society hero"></img>
               </div>
           </div>
-       {/**here 
-      <div className='top-container'>
-        <div className="home-info">
-          <div className="title">
-            <span className='line1' style={{color:'#9D62CC'}}>Western</span> <br />
-            <span className='line2' style={{color:'#60468C'}}>Developers</span> <br />
-            <span className='line3' style={{color:'#303030'}}>Society</span>
-          </div>
-            <div className="pic">
-              <img src={tech}></img>  
-            </div>
-            <div className="text">
-              Building community, one line of code at a time.
-            </div>
-          </div>
-        </div>
-        */}
       </section>
       </Fade>
       
@@ -102,44 +81,6 @@ export default function Home() {
                 or a seasoned pro, there's a place for you in the club. Join us
                 and help us build the future!
               </p>
-
-{/* 
-            </span>
-            <br></br>
-          <span className='card-bullet-points'> 
-          <Typewriter
-            options={{
-              loop: true,
-              delay: 8,
-            }}
-            onInit={(typewriter) => {
-              typewriter.typeString('>We are a community of developers, designers, consultants, and engineers')
-              .pauseFor(2500)
-              .deleteAll(2)
-              typewriter.typeString('>We are Westerns Fastest Growing Tech Club')
-              .pauseFor(2500)
-              .deleteAll(2)
-              typewriter.typeString('>We are club for all students interested in technology')
-              .pauseFor(2500)
-              .deleteAll(2)
-              typewriter.typeString('>We are a collective of students, applying our skills to support our community')
-              .pauseFor(2500)
-              .deleteAll(2)
-              .start();
-            }}
-          />
-            </span>
-          </div>
-          <div className='card-container'>
-            <Slide left>
-            <div className='about-left'>
-            <span className='card-sub-title'>Community First</span>
-            <br></br>
-            Western Developers Society is a club for UWO students who are interested in software development and technology. 
-            WDS is the place for students to discuss new technologies, share knowledge, and work on projects together. 
-            We also host workshops, speakers, and competitions throughout the year to help members learn new skills and keep up with the latest trends in software development. 
-            Whether you're a beginner or a seasoned pro, there's a place for you in the club. Join us and help us build the future!
-**/}
             </div>
           </Slide>
           <Slide right>
