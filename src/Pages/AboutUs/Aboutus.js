83f8ca8b42b5d6fc5e84d67e18f7b1067f12c853
Maaz Ali <maaz@Maazs-MacBook-Air.local>
2022-11-14 23:09:52 -0500

    fixed aboutus spacing

--- src/Pages/AboutUs/Aboutus.js
+++ src/Pages/AboutUs/Aboutus.js
@@ -6,7 +6,6 @@ import Fade from "react-reveal/Fade"
 import DeloitteLogo from "./logos/DeloitteLogo.png"
 import MckinseyLogo from "./logos/MckinseyLogo.png"
 
-
 export default function Aboutus() {
   return (
     <div className="aboutus">
@@ -52,46 +51,42 @@ export default function Aboutus() {
               clubs!
             </p>
           </div>
-          <img
-            className="divider"
-            src="https://anima-uploads.s3.amazonaws.com/projects/62e7f23d5a7d66bdd412e081/releases/62e7f24a41fdb6de5bf9db78/img/divider@1x.svg"
-          />
-
-          <div className="title-2">
-            <h3>Our Partners</h3>
-          </div>
+          <h3 className="title-2">Our Partners</h3>
           <p className="paragraph-2">
             Through our projects, we've had the honour of collaborating with a
             wide range of incredible businesses and institutions. Here are a few
             of them:
           </p>
           <div className="company-container">
-            <div className="company1"><img src={DeloitteLogo} /></div>
-            <div className="company2"><img src={MckinseyLogo} /></div>
-
+            <div className="company1">
+              <img src={DeloitteLogo} />
+            </div>
+            <div className="company2">
+              <img src={MckinseyLogo} />
+            </div>
           </div>
           <div className="flex-row">
-          <Fade>
-            <div className="seeking-partnership-with-us">
-              <h3>Seeking partnership with us?</h3>
-            </div>
-            <p className="paragraphs-3">
-              Western dev society plans on holding both in person and online
-              events for the upcoming 2022-2023 school year. Running live events
-              for the WDS and Western communities comes at a very high cost.
-              Therefore, WDS highly appreaciates and welcomes any financial or
-              in-kind sponsorships
-              <br />
-              <br />
-              There are many benefits that come with collaborating with our
-              association! Partnering with WDS means that we will share and
-              promote your organization&#x27;s marketing material to the Western
-              Engineering &amp; Computer Science and greater Western community.
-              You&#x27;ll also have opportunities to directly engage with the
-              student population through sending representatives from your
-              company, and/or through other collaboration formats. This is great
-              for recruitment purposes!
-            </p>
+            <Fade>
+              <div className="seeking-partnership-with-us">
+                <h3>Seeking partnership with us?</h3>
+              </div>
+              <p className="paragraphs-3">
+                Western dev society plans on holding both in person and online
+                events for the upcoming 2022-2023 school year. Running live
+                events for the WDS and Western communities comes at a very high
+                cost. Therefore, WDS highly appreaciates and welcomes any
+                financial or in-kind sponsorships
+                <br />
+                <br />
+                There are many benefits that come with collaborating with our
+                association! Partnering with WDS means that we will share and
+                promote your organization&#x27;s marketing material to the
+                Western Engineering &amp; Computer Science and greater Western
+                community. You&#x27;ll also have opportunities to directly
+                engage with the student population through sending
+                representatives from your company, and/or through other
+                collaboration formats. This is great for recruitment purposes!
+              </p>
             </Fade>
           </div>
         </div>
