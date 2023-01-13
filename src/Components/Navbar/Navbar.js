c81ffa901daf3aae200a093e48b4535d07d64177
LucasHartman <lucas.hartman@kognitive.ca>
2023-01-13 15:14:25 -0500

    Added mobile navbar when screen width is 768px or less

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,6 +4,20 @@ import { Link } from "react-router-dom"
 import logo from "./logo.png"
 
 function Navbar() {
+
+  function toggleMobileNav() {
+    let linksContainer = document.querySelector('.links-container');
+    let hamburger = document.querySelector('.hamburger');
+    let currentClass = linksContainer.getAttribute('class');
+    if (currentClass.includes('active')) {
+      linksContainer.setAttribute('class', 'links-container');
+      hamburger.setAttribute('class', 'hamburger');
+    } else {
+      linksContainer.setAttribute('class', 'links-container active');
+      hamburger.setAttribute('class', 'hamburger active');
+    }
+  }
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
@@ -12,18 +26,19 @@ function Navbar() {
             <img src={logo} alt="Dev Society Logo"></img>
           </Link>
         </div>
+        <button className="hamburger" onClick={toggleMobileNav}></button>
         <div className="links-container">
           <ul>
-            <li>
+            <li onClick={toggleMobileNav}>
               <Link to="/Contact">contact us</Link>
             </li>
-            <li>
+            <li onClick={toggleMobileNav}>
               <Link to="/JoinUs">join us</Link>
             </li>
-            <li>
+            <li onClick={toggleMobileNav}>
               <Link to="/AboutUs">about us</Link>
             </li>
-            <li>
+            <li onClick={toggleMobileNav}>
               <Link to="/Events">events</Link>
             </li>
             {/**<li>
