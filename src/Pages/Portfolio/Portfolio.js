58756e5e0cf666ee13b7bdd532713ba76baae2b4
Brayden Thompson <bthomp89@uwo.ca>
2023-07-30 20:00:11 -0400

    portfolio mock up

--- src/Pages/Portfolio/Portfolio.js
+++ src/Pages/Portfolio/Portfolio.js
@@ -1,50 +1,39 @@
 import React from "react";
-import Carousel from "react-material-ui-carousel";
-import { Paper, Button } from "@mui/material";
-
 import "./Portfolio.css";
+import Footer from "../../Components/Footer/Footer";
 
 function Portfolio() {
-  const items = [
-    {
-      name: "Project 1",
-      team_leads: "Lead 1, Lead 2",
-      devs: "Dev 1, Dev 2, Dev 3",
-      link: "https://www.instagram.com/p/CgHXZNxpE3d/?hl=en",
-    },
-    {
-      name: "Project 2",
-      team_leads: "Lead 1, Lead 2",
-      devs: "Dev 1, Dev 2, Dev 3",
-      link: "https://www.instagram.com/p/ChVFERGJy5E/?hl=en",
-    },
-  ];
-
-  //document.body.style = 'background: #D9DDF5;';
   return (
-    <div className="page">
-      <h1>Scroll to View Our Portfolio</h1>
-      <div className="proj-carousel">
-        <Carousel className="carousel">
-          {items.map((item, i) => (
-            <Item key={i} item={item} />
-          ))}
-        </Carousel>
+    <div className="port-container">
+      <div className="port-title">Portfolio</div>
+      <div className="grid-container">
+        <div className="grid-row">
+          <div className="grid-item" data-text="Info About Project">
+            Project Name
+          </div>
+          <div className="grid-item" data-text="Info About Project">
+            Project Name
+          </div>
+        </div>
+        <div className="grid-row">
+          <div className="grid-item" data-text="Info About Project">
+            Project Name
+          </div>
+          <div className="grid-item" data-text="Info About Project">
+            Project Name
+          </div>
+        </div>
+        <div className="grid-row">
+          <div className="grid-item" data-text="Info About Project">
+            Project Name
+          </div>
+          <div className="grid-item" data-text="Info About Project">
+            Project Name
+          </div>
+        </div>
       </div>
+      <Footer></Footer>
     </div>
-  );
-}
-
-function Item(props) {
-  return (
-    <Paper className="paper">
-      <h2 className="proj-name">{props.item.name}</h2>
-      <p className="proj-team-leads">{props.item.team_leads}</p>
-      <p className="proj-devs">{props.item.devs}</p>
-      <a className="proj-link" href={props.item.link}>
-        Link: {props.item.link}
-      </a>
-    </Paper>
   );
 }
 
