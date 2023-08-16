8ded66e77b62e92c47265e66d78e2ebbbc09edf4
Tara Lee <taralee@Taras-MacBook-Pro.local>
2023-08-16 16:59:02 -0400

    footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import './Footer.css'
+import {AiFillInstagram } from 'react-icons/ai'
+import {BsLinkedin}from 'react-icons/bs'
+import {FaDiscord} from 'react-icons/fa'
 
 function Footer() {
   return (
     <div>
       <section className="footer">
-        <div>
-          socials
+        <div className='socials'>
+          <a href='https://www.instagram.com/westerndevsociety/' target='_blank' tag='social'> <AiFillInstagram size={25}></AiFillInstagram></a>
+          <a href='https://www.linkedin.com/company/81320984/admin/' target='_blank' tag='social'><BsLinkedin size={25}></BsLinkedin></a>
+          <a href='https://discord.com/invite/QWzvg3XmPz' target='_blank' tag='social' ><FaDiscord size={25}></FaDiscord></a>
         </div>
         <div className="info">
             <strong>Western Developers Society </strong><br/>
